c786666689708c75c8ecb8e6db923a6ad37cdead
winthrop gutmann <winthrop.gutmann@arcadiasolutions.com>
2016-02-10 22:09:24 -0500

    i really just dont want to lose this

--- LocalSettings/src/ulc.scala
+++ LocalSettings/src/ulc.scala
@@ -5,24 +5,9 @@ import scala.util.matching.Regex
 
 object ulc {
   def main(branch:String, custom:String, username:String): Unit = {
-    
-    var myClass = new Setting(branch, custom, username)
-    println(myClass.getVersion)
-    
-  } //EO MAIN
-  
-  class Setting(var Branch: String, Custom:String, Username:String){
-    this.setVersion(Branch)
-    
-    protected var version = ""
-    
-    def setVersion(branch:String){
-      val pattern = new Regex("[0-9]");
-      this.version = (pattern findAllIn branch).mkString
-    }
-    
-    def getVersion():String={ return this.version}
-    
+    var myString= "hello world"
+    println(myString)
+    println(myString.substring(1))
   }
 
-}//end of object
+}
--- helloworld/src/hello.scala
+++ helloworld/src/hello.scala
@@ -7,47 +7,104 @@ object tst {
   
   def main(args: Array[String]): Unit = {
     //variables
-      var custom = args(1)
-      var user = args(2)
-      val version = 0
-      var dbname = if(args(1).replace(".", "").replace("-", "") =="") args(0).replace(".", "").replace("-", "") else args(1).replace(".", "").replace("-", "")
-      val writer = new PrintWriter(new File("C:\\Users\\" + user + "\\dev\\qdw-deployment\\Build\\LocalSettings.rb"))
-      println(dbname)
-      val sourceFile = "C:\\Users\\"+user+"\\dev\\qdw-deployment\\Build\\LocalSettings2.rb"
+    var custom = args(1)
+    var user = args(2)
+    val mpi = args(3)
+    val web = args(4)
+    val version = 0
+    var dbname = if(args(1).replace(".", "").replace("-", "") =="") args(0).replace(".", "").replace("-", "") else args(1).replace(".", "").replace("-", "")
+    val writer = new PrintWriter(new File("C:\\Users\\" + user + "\\dev\\qdw-deployment\\Build\\winTstMe.rb"))
+    val sourceFile = "C:\\Users\\"+user+"\\dev\\qdw-deployment\\Build\\LocalSettings.rb"
+    
+    //functions
+    def checkForComment(line:String):Boolean={
+      if(line.charAt(0).equals('#') && line.length() !=0){ 
+        writeLine(line)
+        println( "writing " + line)
+        return true
+      }else{
+        println("return false")
+        return false
+      }
+    }
+      
+    def writeLine(line:String){
+      println("writing line: " + line +"  ****")
+      writer.write(line + "\n")
+    }
 
-      if(dbname.toLowerCase == "master"){val version = getVersionCheck(sourceFile); println(version)}
-      
-      for(line <- Source.fromFile(sourceFile).getLines()){
-        if(!checkForComment(line)){
-          checkForProvisioningClient()
+    def getVersionCheck(file:String):Int={
+      println("checking version")
+      val bufferSource = Source.fromFile(file)
+      for (line <- bufferSource.getLines) {
+        println(line)
+        if (line.contains("master_version:")){
+          val pattern = new Regex("[0-9]");
+          bufferSource.close
+          return (pattern findAllIn line).mkString.toInt
         }
       }
+      bufferSource.close
+      return 0
+    }
       
       
-      
-      
-      
-      //functions
-      def checkForComment(line:String):Boolean={if(line.charAt(0).equals('#')){ writeLine(line); return true}else{return false}}
-      
-      def writeLine(line:String){writer.write(line + "\n")}
+   def checkForProvisioningClient(line : String, dbname:String){
+    if(line.contains("provisioning_client")){         
+      var myVar = line.split(":")
+      if(myVar(1).contains("\"")){
+        var writethis = "  provisioning_client: \"" + dbname + "\"," + "\n" 
+        writeLine(writethis)
+      }
+    }else{
+        writeLine(line)
+    }
+  }
+     
+     
+  def checkForWeb(line:String, web:String){
+    if(line.contains("deploy_layers") && web.toLowerCase() == "y")
+      writeLine("  #deploy_layers: %w(Staging WAREHOUSE), #comment out to deploy web_warehouse_dev")
+    else
+      writeLine("  deploy_layers: %w(Staging WAREHOUSE), #comment out to deploy web_warehouse_dev")
+  }
+
   
-      def getVersionCheck(file:String):Int={
-        println("incheck")
-        var myVersion = Array[String]()
-        for (line <- Source.fromFile(file).getLines()){
-          if (line.contains("#master_version:")){
-            val pattern = new Regex("[0-9]");
-            return (pattern findAllIn line).mkString.toInt
-          }
+  def checkForRepository(line:String, dbname:String, mpi:String, version:Int){
+    if(line.contains("repositories")){
+      var writeThis = "  repositories: [:core"
+      if(version > 47) writeThis = writeThis + ", :client"
+      if(mpi.toLowerCase() =="y") writeThis = writeThis + ", :mpi"
+      writeThis = writeThis + "],"
+      writeLine(writeThis)
+    }
+  }
+    
+    
+    
+    
+      //beginning of program  
+      println("custom: " +custom +" user: "+ user +" mpi: " + mpi + " web :" + web + " version : " + version + " dbname: "+ dbname)
+      if(dbname.toLowerCase == "master")
+      {
+        val version = getVersionCheck(sourceFile)
+        println("in master: " + version)
+      }
+      
+      val source = Source.fromFile(sourceFile)
+      println("Before for")
+      for (line <- source.getLines) {
+        println(line)
+        if(!checkForComment(line)){
+          println("this is not a comment: ")
+//          checkForProvisioningClient(line, dbname)
+//          checkForWeb(line, web)
+//          checkForRepository(line, dbname, mpi, version)
         }
-        return 0
       }
       
-      
-      def checkForProvisioningClient(){
-        
-      }
-      
+
+      writer.close()
+      println("end of main")
   } //end of main
 } //end object
